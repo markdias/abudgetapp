dc6c68004f6ddab6c28ca05cd3523038c529113b
markdias <mark@mdias.co.uk>
2025-03-01 10:01:24 +0000

    Initial Commit

---
+++ abudgetapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  abudgetapp
+//
+//  Created by Mark Dias on 01/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ abudgetapp/abudgetappApp.swift
@@ -0,0 +1,17 @@
+//
+//  abudgetappApp.swift
+//  abudgetapp
+//
+//  Created by Mark Dias on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct abudgetappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
